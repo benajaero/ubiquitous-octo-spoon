c2b41c5182c2ab5fef3c40c38f77da1ba9f49573
Blaze349 <Blaze349@protonmail.com>
2017-07-31 21:33:44 +0930

    Add variables

--- main.cpp
+++ main.cpp
@@ -2,13 +2,36 @@
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
 
+char memory[4096];
+char V[16]; //data registers
+short I; //address register
+short stack[24];
+int timer;
+int soundTimer;
+char keyboard[16];
+
+void opcode();
+void draw();
+void input();
 
 int main() {
-    char memory[4096];
-    char V[16]; //data registers
-    short I; //address register
-    short stack[24];
-    int timer;
-    int soundTimer;
+    
+    while(true) {
+        opcode();
+        draw();
+        input();
+    }
     return 0;
 }
+
+void draw() {
+
+}
+
+void input() {
+
+}
+void opcode() {
+
+}
+
