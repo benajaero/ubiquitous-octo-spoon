93ee32490ae4d5c9626116b4a59759d63802b0f6
Blaze349 <blaze349@protonmail.com>
2017-08-07 22:29:21 +0930

    Finish for today

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@ class chip8 {
         unsigned int soundTimer;
         unsigned char keyboard[16];
         unsigned char gfx[2048];
+        sf::Clock clock;
+        double fps = 1e6f/60.f;
+        bool drawFlag = 0;
         unsigned char fontSet[80] = {
           0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
           0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -47,7 +50,36 @@ class chip8 {
         void loop(sf::Window& window) {
             while(window.isOpen()) {
                 input(window);
-                opcode();
+                opcode(window);
+                if (clock.getElapsedTime().asMicroseconds() > fps) {
+                    clock.restart();
+                    soundTimer--;
+                    timer--;
+                }
+                if (drawFlag) {
+                    drawScreen();
+                }
+            }
+        }
+        void drawScreen() {
+            float xtileoffset = 800.0/64.0;
+            float ytileoffset = 600.0/32.0;
+            for (int y = 0; y < 32; y++) {
+                for (int x = 0; x < 64; x++) {
+                    sf::VertexArray tile(sf::Quads, 4);
+                    tile[0].position = sf::Vector2f(x * xtileoffset, y * ytileoffset);
+                    tile[0].position = sf::Vector2f((x + 1) * xtileoffset, y * ytileoffset);
+                    tile[0].position = sf::Vector2f(x * xtileoffset, (y + 1) * ytileoffset);
+                    tile[0].position = sf::Vector2f((x + 1) * xtileoffset, (y + 1) * ytileoffset);
+                    sf::Color color;
+                    if (gfx[x + (y * 64)] == 1) {
+                        color.r = 0; color.b = 0; color.g = 0;
+                    }
+                    else color = sf::Color::White; 
+                    for (int i = 0; i < 4; i++) {
+                        tile[i].color = color;
+                    }
+                }
             }
         }
         sf::Keyboard::Key translateToSfml(int x) {
@@ -105,6 +137,62 @@ class chip8 {
                     exit(3);
             }
         }
+        int translateToKeys(sf::Keyboard::Key key) {
+            switch(key) {
+                case sf::Keyboard::Num1:
+                    return 0; 
+                break;
+                case sf::Keyboard::Num2:
+                    return 1; 
+                break;
+                case sf::Keyboard::Num3: 
+                    return 3;
+                break;
+                case sf::Keyboard::Num4:
+                    return 4; 
+                break;
+                case sf::Keyboard::Q:
+                    return 5;
+                break;
+                case sf::Keyboard::W:
+                    return 6;
+                break;
+                case sf::Keyboard::E:
+                    return 7;
+                break;
+                case sf::Keyboard::R:
+                    return 8;
+                break;
+                case sf::Keyboard::A:
+                    return 9;
+                break;
+                case sf::Keyboard::S:
+                    return 10;
+                break;
+                case sf::Keyboard::D:
+                    return 11;
+                break;
+                case sf::Keyboard::F:
+                    return 12;
+                break;
+                case sf::Keyboard::Z:
+                    return 13;
+                break;
+                case sf::Keyboard::X:
+                    return 14;
+                break;
+                case sf::Keyboard::C:
+                    return 15;
+                break;
+                case sf::Keyboard::V:
+                    return 16;
+                break;
+                default:
+                    std::cerr << "Keyboard condition not checked" << std::endl;
+                    exit(3);
+            }
+        }
+
         void keyboardInput(int x) {
             keyboard[x] = sf::Keyboard::isKeyPressed(translateToSfml(x));
         } 
@@ -136,7 +224,20 @@ class chip8 {
         }
 
         void draw(char x, char y, char height) {
+            unsigned short pixel;
+            V[0xF] = 0;
 
+            for (int yline = 0; yline < height; yline++) {
+                pixel = memory[I + yline];
+                for (int xX = 0; xX < 8; xX++) {
+                    if ((pixel & (0x80 >> xX)) != 0) {
+                        if (gfx[x + xX + ((y + yline) * 64)] == 1) V[0xF] = 1;
+                        gfx[x + xX + ((y + yline) * 64)] ^= 1;
+                    } 
+                }
+            }
+
+            drawFlag = true;
         }
 
         void input(sf::Window& window) {
@@ -145,12 +246,14 @@ class chip8 {
                 if (event.type == sf::Event::Closed) window.close();
             }
         }
-        void opcode() {
+        void opcode(sf::Window& window) {
             unsigned short opcode = memory[pc] << 8 | memory[pc+1];
             unsigned int x = (opcode & 0x0F00) >> 4;
             unsigned int y = (opcode & 0x00F0) >> 8;
             unsigned int NNN = opcode & 0x0FFF;
             unsigned int NN = opcode & 0x00FF;
+            bool rightEvent = 0;
+            sf::Event event;
 
             switch (opcode & 0xF000) {
                 case 0x0000:
@@ -281,12 +384,12 @@ class chip8 {
                 case 0xE000:
                     switch(opcode & 0x00FF) {
                         case 0x009E:
-                            keyboardInput(x);
+                            keyboardInput(V[x]);
                             if (keyboard[V[x]] == 1) pc += 4;
                             else pc += 2;
                         break;
                         case 0x00A1:
-                            keyboardInput(x);
+                            keyboardInput(V[x]);
                             if (keyboard[V[x]] == 0) pc += 4;
                             else pc += 2;
 
@@ -307,6 +410,16 @@ class chip8 {
 
                         case 0x000A:
                            //wait for key 
+                           do {
+                               while(window.pollEvent(event)) {
+                                   if (event.type == sf::Event::KeyPressed) {
+                                       V[x] = translateToKeys(event.key.code); 
+                                       rightEvent = 1;
+                                       pc += 2;
+                                       break;
+                                   }
+                               }
+                           } while (!rightEvent);
                         break;
 
                         case 0x0015:
@@ -326,10 +439,30 @@ class chip8 {
 
                         case 0x0029:
                             //complete later
+                            I = 0x050 + V[x] * 5;
+                            pc += 2;
+                        break;
+                        
+                        case 0x0033:
+                            memory[I] = V[x] / 100;
+                            memory[I + 1] = (V[x] / 10) % 10;
+                            memory[I + 2] = (V[x] % 100) % 10;
                             pc += 2;
                         break;
 
+                        case 0x0055:
+                            for (int i = 0; i < 16; i++) {
+                                memory[I + i] = V[i];
+                            }
+                            pc += 2;
+                        break;
 
+                        case 0x0065:
+                            for (int i = 0; i < 16; i++) {
+                                V[i] = memory[I + i];
+                            }
+                            pc += 2;
+                        break;
                     }
                 break;
                 default:
